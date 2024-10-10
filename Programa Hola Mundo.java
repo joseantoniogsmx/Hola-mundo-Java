c4c281b72140d9a9d9df3330695258fc216710d6
joseantoniogsmx <joseantoniogsmx@gmail.com>
2024-10-10 16:23:19 -0600

    Create Programa Hola Mundo.java

---
+++ Programa Hola Mundo.java
@@ -0,0 +1,6 @@
+public class ProgramaHolaMundo {
+    public static void main(String[] args) {
+        // visualizar un mensaje de saludo
+        System.out.println("¡Hola, mundo!");
+    } // fin del método main
+} // fin del ProgramaHolaMundo

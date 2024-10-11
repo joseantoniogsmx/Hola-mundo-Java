7c800fa936df88f09a6e68dd84f787fa312a7bc8
joseantoniogsmx <joseantoniogsmx@gmail.com>
2024-10-10 18:58:00 -0600

    Update Programa Hola Mundo.java
    Cómo compilar?

--- Programa Hola Mundo.java
+++ Programa Hola Mundo.java
@@ -1,5 +1,7 @@
-public class ProgramaHolaMundo {
-    public static void main(String[] args) {
+public class ProgramaHolaMundo
+    {
+    public static void main(String[] args)
+        {
         // visualizar un mensaje de saludo
         System.out.println("¡Hola, mundo!");
     } // fin del método main
